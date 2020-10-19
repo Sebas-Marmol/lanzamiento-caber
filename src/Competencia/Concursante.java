45a10d468c10840347cc954fcf81acc05fecf430
Sebastián Mármol <sebastian.marmol@virtualdreams.io>
2020-10-19 08:56:11 -0300

    Leer archivo y calcular dist efectiva

--- src/Competencia/Concursante.java
+++ src/Competencia/Concursante.java
@@ -1,5 +1,16 @@
 package Competencia;
 
+import java.util.ArrayList;
+
 public class Concursante {
 
+	private ArrayList<Lanzamiento> lanzamientos;
+
+	public Concursante() {
+		this.lanzamientos = new ArrayList<Lanzamiento>();
+	}
+
+	public void registrarLanzamiento(Lanzamiento nuevoLanzamiento) {
+		this.lanzamientos.add(nuevoLanzamiento);
+	}
 }
--- src/Competencia/Concurso.java
+++ src/Competencia/Concurso.java
@@ -1,13 +1,36 @@
 package Competencia;
 
+import java.io.File;
+import java.util.Scanner;
+
 public class Concurso {
 
-	private Concursante[] consursantes;
+	private Concursante[] concursantes;
 	private int[] ganadoresDistancia;
 	private int[] ganadoresConsistencia;
 
 	public Concurso() {
 		ganadoresDistancia = new int[3];
-		ganadoresConsistencia= new int[3];
+		ganadoresConsistencia = new int[3];
+	}
+
+	public void obtenerConcursantesArchivo() {
+		Scanner arch = null;
+		try {
+			arch = new Scanner(new File("in/DatosConcurso.in"));
+			int participantes = arch.nextInt();
+			this.concursantes = new Concursante[participantes];
+			for (int i = 0; i < participantes; i++) {
+				Concursante conc=new Concursante();
+				conc.registrarLanzamiento(new Lanzamiento(arch.nextDouble(),arch.nextDouble()));
+				conc.registrarLanzamiento(new Lanzamiento(arch.nextDouble(),arch.nextDouble()));
+				conc.registrarLanzamiento(new Lanzamiento(arch.nextDouble(),arch.nextDouble()));
+				concursantes[i]=conc;
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			arch.close();
+		}
 	}
 }
